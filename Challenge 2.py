3619e0be9bb80a94ab8de1166f8fdfe65565aeaf
Ram123sa <148320602+Ram123sa@users.noreply.github.com>
2023-10-18 13:16:38 +0530

    Add files via upload

--- Challenge 2.py
+++ Challenge 2.py
@@ -1,23 +1,9 @@
-class Student:
-  def __init__(self, name, roll_number, cgpa):
-    self.name = name
-    self.roll_number = roll_number
-    self.cgpa = cgpa
+def CheckLeap(Year):
+  if ((Year % 400 == 0) or (Year % 100 != 0) and (Year % 4 == 0)):
+    print("Given Year is a leap Year")
+  else:
+    print("Given Year is not a leap Year")
 
-  def sort_students(student_list):
-    sorted_students = sorted(student_list,key=lambda student: student.cgpa,reverse=True)
-    return sorted_students
 
-students = [
-    Student("Manikandan", "A123", 7.8),
-    Student("Madhankumar", "A124", 8.9),
-    Student("Alagu manikandan", "A125", 9.1),
-    Student("Saran", "A126", 9.7),
-    Student("Tamilselvan","A127", 9.8),
-    Student("Manogaran","A128", 9.9),
-]
-
-sorted_students = Student.sort_students(students)
-
-for student in sorted_students:
-    print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
+Year = int(input("Enter the year:"))
+CheckLeap(Year)
