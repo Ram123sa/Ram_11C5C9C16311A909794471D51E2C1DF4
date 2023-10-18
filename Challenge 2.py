4091ea60b5d2fa973a525ef79a12e1b489505970
Ram123sa <148320602+Ram123sa@users.noreply.github.com>
2023-10-18 13:06:32 +0530

    Add files via upload

--- Challenge 2.py
+++ Challenge 2.py
@@ -1,9 +1,13 @@
-def CheckLeap(Year):
-  if ((Year % 400 == 0) or (Year % 100 != 0) and (Year % 4 == 0)):
-    print("Given Year is a leap Year")
-  else:
-    print("Given Year is not a leap Year")
-
-
-Year = int(input("Enter the year:"))
-CheckLeap(Year)
+class Player:
+  def play(self):
+      print("The player is playing cricket.")
+class Batsman(Player):
+  def play(self):
+      print("The batsman is batting.")
+class Bowler(Player):
+  def play(self):
+      print("The bowler is bowling.")
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
